4a8b0fd7f6d1650fb434a07625539df4bf1639ee
Diego Pereira <diegogomespereira@gmail.com>
2020-06-22 09:31:05 -0400

    Resolve folder name casing conflict

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react'
 
-import Info from '../components/user/Info'
-import Repos from '../components/user/Repos'
+import Info from '../components/profile/Info'
+import Repos from '../components/profile/Repos'
 
 export default function Profile() {
   return (
